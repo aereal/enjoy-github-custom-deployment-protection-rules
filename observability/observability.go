d1612a2bf4f6a21647dc901fd6de04c5dc759b9c
aereal <aereal@aereal.org>
2023-08-23 19:57:20 +0900

    fix: match schema version with lambda detector

--- observability/observability.go
+++ observability/observability.go
@@ -12,7 +12,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.18.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
 type setupConfig struct {
