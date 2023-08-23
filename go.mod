a141d2d8ad3f6ae52a61560618aee778cac04f6e
aereal <aereal@aereal.org>
2023-08-23 22:28:51 +0900

    feat: handle /webhook endpoint

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/aereal/otel-propagators/datadog v0.2.0
 	github.com/aws/aws-lambda-go v1.41.0
+	github.com/google/go-github/v54 v54.0.0
 	go.opentelemetry.io/contrib/detectors/aws/lambda v0.42.0
 	go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda v0.42.0
 	go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda/xrayconfig v0.42.0
@@ -17,20 +18,26 @@ require (
 )
 
 require (
+	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cloudflare/circl v1.3.3 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.16.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.16.0 // indirect
 	go.opentelemetry.io/otel/metric v1.16.0 // indirect
 	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/oauth2 v0.11.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
 	google.golang.org/grpc v1.55.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- webhook/webhook.go
+++ webhook/webhook.go
@@ -3,14 +3,18 @@ package webhook
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"mime"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/aereal/enjoy-github-custom-deployment-protection-rules/log"
 	"github.com/aereal/enjoy-github-custom-deployment-protection-rules/observability"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/google/go-github/v54/github"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda/xrayconfig"
 	"go.opentelemetry.io/otel"
@@ -22,6 +26,21 @@ import (
 var (
 	serviceVersion   string
 	exportTraceGrace = time.Second * 3
+	respOK           = &events.LambdaFunctionURLResponse{
+		StatusCode: http.StatusOK,
+		Headers:    map[string]string{"content-type": "application/json"},
+		Body:       `{}`,
+	}
+	respError = &events.LambdaFunctionURLResponse{
+		StatusCode: http.StatusInternalServerError,
+		Headers:    map[string]string{"content-type": "application/json"},
+		Body:       `{"error":"failed"}`,
+	}
+	respNotFound = &events.LambdaFunctionURLResponse{
+		StatusCode: http.StatusNotFound,
+		Headers:    map[string]string{"content-type": "application/json"},
+		Body:       `{"error":"not found"}`,
+	}
 )
 
 func New() *Handler {
@@ -38,6 +57,30 @@ type Handler struct {
 func (h *Handler) Handle(ctx context.Context, req events.LambdaFunctionURLRequest) (_ *events.LambdaFunctionURLResponse, err error) {
 	ctx, span := h.tracer.Start(ctx, "Handler.Handle", trace.WithTimestamp(time.Unix(req.RequestContext.TimeEpoch/1000, req.RequestContext.TimeEpoch%1000)))
 	defer func() {
+		span.SetStatus(codes.Ok, "")
+		span.End()
+	}()
+	sc := span.SpanContext()
+	logger := log.FromContext(ctx).With(zap.Stringer("trace_id", sc.TraceID()), zap.Stringer("span_id", sc.SpanID()))
+	ctx = log.WithLogger(ctx, logger)
+	defer func() {
+		logger.Info("processed request", zap.String("path", req.RawPath))
+	}()
+
+	switch req.RawPath {
+	case "/webhook":
+		if err := h.handleWebhook(ctx, req); err != nil {
+			return respError, nil
+		}
+		return respOK, nil
+	default:
+		return respNotFound, nil
+	}
+}
+
+func (h *Handler) handleWebhook(ctx context.Context, req events.LambdaFunctionURLRequest) (err error) {
+	ctx, span := h.tracer.Start(ctx, "Handler.handleWebhook")
+	defer func() {
 		code := codes.Ok
 		if err != nil {
 			code = codes.Error
@@ -46,20 +89,71 @@ func (h *Handler) Handle(ctx context.Context, req events.LambdaFunctionURLReques
 		span.SetStatus(code, "")
 		span.End()
 	}()
-	sc := span.SpanContext()
-	logger := log.FromContext(ctx).With(zap.Stringer("trace_id", sc.TraceID()), zap.Stringer("span_id", sc.SpanID()))
-	logger.Info("handle request", zap.String("path", req.RawPath))
 
-	return respNotFound, nil
+	payload, err := parseAndValidateWebhookPayload(req, []byte("83a196e5cf55aeaf98e85c1eb08a0c14feb30ae6f7d9a8db79d20b705c7244ef"))
+	if err != nil {
+		return fmt.Errorf("parseAndValidateWebhookPayload: %w", err)
+	}
+	logger := log.FromContext(ctx)
+	switch payload := payload.(type) {
+	case *github.DeploymentProtectionRuleEvent:
+		fields := []zap.Field{
+			zap.Stringp("environment", payload.Environment),
+			zap.Stringp("action", payload.Action),
+			zap.Stringp("deployment_callback_url", payload.DeploymentCallbackURL),
+		}
+		if inst := payload.Installation; inst != nil {
+			fields = append(fields,
+				zap.Int64p("installation.target_id", inst.TargetID),
+				zap.Stringp("installation.target_type", inst.TargetType),
+				zap.Int64p("installation.id", inst.ID))
+		}
+		if sender := payload.Sender; sender != nil {
+			fields = append(fields, zap.Stringp("sender.login", sender.Login))
+		}
+		if deploy := payload.Deployment; deploy != nil {
+			fields = append(fields,
+				zap.Stringp("deploy.sha", deploy.SHA),
+				zap.Stringp("deploy.ref", deploy.Ref),
+				zap.Stringp("deploy.task", deploy.Task),
+				zap.Stringp("deploy.environment", deploy.Environment),
+				zap.Stringp("deploy.description", deploy.Description),
+				zap.Int64p("deploy.id", deploy.ID))
+		}
+		logger.Info("receive deployment_protection_rule event", fields...)
+	default:
+		logger.Info("receive unknown webhook event", zap.String("github.webhook.go_type", fmt.Sprintf("%T", payload)))
+	}
+
+	return nil
 }
 
-var (
-	respNotFound = &events.LambdaFunctionURLResponse{
-		StatusCode: http.StatusNotFound,
-		Headers:    map[string]string{"content-type": "application/json"},
-		Body:       `{"error":"not found"}`,
+func parseAndValidateWebhookPayload(req events.LambdaFunctionURLRequest, secret []byte) (any, error) {
+	reqHeader := http.Header{}
+	for k, v := range req.Headers {
+		reqHeader.Add(k, v)
 	}
-)
+	sig := reqHeader.Get(github.SHA256SignatureHeader)
+	if sig == "" {
+		sig = reqHeader.Get(github.SHA1SignatureHeader)
+	}
+	mt, _, err := mime.ParseMediaType(reqHeader.Get("content-type"))
+	if err != nil {
+		return nil, fmt.Errorf("mime.ParseMediaType: %w", err)
+	}
+
+	body := strings.NewReader(req.Body)
+	payload, err := github.ValidatePayloadFromBody(mt, body, sig, secret)
+	if err != nil {
+		return nil, fmt.Errorf("github.ValidatePayloadFromBody: %w", err)
+	}
+	webhookType := reqHeader.Get(github.EventTypeHeader)
+	parsed, err := github.ParseWebHook(webhookType, payload)
+	if err != nil {
+		return nil, fmt.Errorf("github.ParseWebHook: %w", err)
+	}
+	return parsed, nil
+}
 
 func Start() int {
 	logger, err := log.New()
